a40cfb6fde04b3f855399becdeba213cbe7bb23c
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-06-25 12:37:44 +0200

    responsiveness

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -56,26 +56,35 @@ function Projects() {
   return (
     <div className={style.mainContainer}>
       <Row style={{ justifyContent: "center" }}>
-        <Col md={6} sm={24}>
-          <Avatar
-            shape="square"
-            size={264}
-            src={<Image preview={false} src={logo} />}
-          />
-          <div className={style.miniBioContainer}>
-            <div>
-              <div className={style.miniBio}>{t("miniBio1")}</div>
-              <div className={style.miniBio}>{t("miniBio2")}</div>
-              <div className={style.miniBio}>{t("miniBio3")}</div>
-              <div className={style.miniBio}>
-                <a href="mailto:progetti.rotondo@gmail.com">
-                  progetti.rotondo@gmail.com
-                </a>
-              </div>
+        <Col sm={24} md={24} lg={6}>
+          <Row className={style.rowC}>
+            <div className={style.rowC}>
+              <Col xs={24} sm={24} md={24}>
+                <Avatar
+                  style={{ textAlign: "center" }}
+                  shape="square"
+                  size={264}
+                  src={<Image preview={false} src={logo} />}
+                />
+              </Col>
+              <Col xs={24} sm={24} md={24}>
+                <div className={style.miniBioContainer}>
+                  <div>
+                    <div className={style.miniBio}>{t("miniBio1")}</div>
+                    <div className={style.miniBio}>{t("miniBio2")}</div>
+                    <div className={style.miniBio}>{t("miniBio3")}</div>
+                    <div className={style.miniBio}>
+                      <a href="mailto:progetti.rotondo@gmail.com">
+                        progetti.rotondo@gmail.com
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </Col>
             </div>
-          </div>
+          </Row>
         </Col>
-        <Col md={18} sm={24}>
+        <Col sm={24} md={24} lg={18}>
           <div className={style.projectsContainer}>
             <Card loading={loading} bordered={false}>
               <Row>{showProjects()}</Row>
