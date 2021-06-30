55424f89a6c214bda757c204c43b07abb887a0f1
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-06-30 18:03:24 +0200

    lazy load

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -2,54 +2,27 @@ import { Avatar, Card, Col, Image, Row } from "antd";
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router";
 // @ts-ignore
 import logo from "../../images/LOGO.jpg";
 import { projectsThunk } from "../../redux/actions";
+import LazyImage from "../lazyImage";
 import style from "./style.module.scss";
 
 function Projects() {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const history = useHistory();
   const projectsData = useSelector((state) => state.projects.projects);
   const loading = useSelector((state) => state.projects.loading);
 
   const showProjects = () => {
-    let a;
     if (
       loading === false &&
       projectsData !== undefined &&
       projectsData !== null
     ) {
       const proj = Object.keys(projectsData).map((key) => [projectsData[key]]);
-      a = proj.map((e) =>
-        e.map((l) => (
-          <Col md={8} sm={24} key={l._id}>
-            <div className={style.imageContainer}>
-              <div
-                className={style.image}
-                onClick={() => {
-                  history.push(`/project/${l._id}`);
-                }}
-              >
-                <Image preview={false} alt={l.title} src={l.images[0]} />
-                <div className={style.imageDescription}>
-                  <div>
-                    {l.subtitle !== "" ? (
-                      <div>{`${l.title} - ${l.subtitle}`}</div>
-                    ) : (
-                      <div>{l.title}</div>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Col>
-        ))
-      );
+      return proj.map((e) => e.map((l) => <LazyImage data={l} />));
     }
-    return a;
   };
 
   useEffect(() => {
