8c4818326048c9b142b45ff63f0d28681442d5c0
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-08-26 22:05:56 +0200

    add fly as server host

--- src/api.js
+++ src/api.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export const getProjects = async () => {
   const response = await axios.get(
-    "https://rotondo-server.herokuapp.com/projects"
+    "https://rotondo-server.fly.dev/projects"
   );
   return response.data;
 };
 
 export const getProject = async ({ id }) => {
   const response = await axios.get(
-    `https://rotondo-server.herokuapp.com/project/${id}`
+    `https://rotondo-server.fly.dev/project/${id}`
   );
   return response.data;
 };
