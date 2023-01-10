a9804bcf37cfe66ddd42dc692957bf126dacd65a
Linda Vitali <linda.vitali2@studio.unibo.it>
2023-01-10 16:27:15 +0100

    chore: edit server url

--- src/api.js
+++ src/api.js
@@ -1,17 +1,13 @@
 import axios from "axios";
 
 export const getProjects = async () => {
-  const response = await axios.get(
-    // "https://rotondo-server.fly.dev/projects"
-    "http://localhost:3000/projects"
-  );
+  const response = await axios.get("https://rotondo-server.fly.dev/projects");
   return response.data;
 };
 
 export const getProject = async ({ id }) => {
   const response = await axios.get(
-    // `https://rotondo-server.fly.dev/project/${id}`
-    `http://localhost:3000/project/${id}`
+    `https://rotondo-server.fly.dev/project/${id}`
   );
   return response.data;
 };
