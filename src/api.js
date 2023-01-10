9aed49cd6bbe33e6ff8853eb6fd90bcc8436a8c8
Linda Vitali <linda.vitali2@studio.unibo.it>
2023-01-10 16:39:32 +0100

    chore: set local server

--- src/api.js
+++ src/api.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 export const getProjects = async () => {
-  const response = await axios.get("https://rotondo-server.fly.dev/projects");
+  const response = await axios.get("http://localhost:3000/projects");
   return response.data;
 };
 
 export const getProject = async ({ id }) => {
-  const response = await axios.get(
-    `https://rotondo-server.fly.dev/project/${id}`
-  );
+  const response = await axios.get(`http://localhost:3000/project/${id}`);
   return response.data;
 };
